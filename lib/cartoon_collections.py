c3dee924722569de796b29f6f61d54ca4376648c
Shukri Hussein <husseinshukri@outlook.com>
2024-04-02 11:52:38 -0400

    lab-complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,25 @@
-def roll_call_dwarves():
-    pass
 
-def summon_captain_planet():
-    pass
+# Using enumerate for roll_call_dwarves to avoid manual index management
+def roll_call_dwarves(dwarf_list):
+    for index, name in enumerate(dwarf_list, start=1):
+        print(f'{index}. {name}')
 
-def long_planeteer_calls():
-    pass
+# Using map and a lambda function for summon_captain_planet for a more functional approach
+def summon_captain_planet(planeteer_calls):
+    return list(map(lambda call: f'{call.title()}!', planeteer_calls))
 
-def find_the_cheese():
-    pass
+# Using any() for long_planeteer_calls to simplify the logic
+def long_planeteer_calls(words):
+    return any(len(word) > 4 for word in words)
+    # for i in range(len(words)):
+        # if len(words[i] > 4):
+    #     return True
+    # return false
+
+# Using next with a generator expression for find_the_cheese for a more concise approach
+cheesez = ["camembert", "gouda", "cheddar"]
+
+def find_the_cheese(foods):
+    return next((food for food in foods if food in cheesez), None)
+
+# words - any word length > 4 for word in words 
